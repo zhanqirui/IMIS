3bef3fa1218e91c2311f4924e5ea70aa092cd10d
wuxichuan <3431549462@qq.com>
2024-12-18 19:04:44 +0800

    更新.gitignore文件，添加IMIS.ksp和public/server.json的忽略规则；将IMIS.ksp和public/server.json中的serverUrl从localhost地址更改为10.0.67.16；修改login.vue文件的模板格式，调整元素的排列；新增patient.vue文件，包含基础模板结构；创建testvue目录并添加.gitkeep文件以保持其存在。

---
+++ src/views/patient.vue
@@ -0,0 +1,18 @@
+<template>
+  <KPage>
+    <KSection>
+      <KBlock>
+        <KCell> <el-image src="/logo.svg" /></KCell>
+      </KBlock>
+      <KBlock :span="5">
+        <KCell> </KCell>
+      </KBlock>
+      <KBlock :span="5">
+        <KCell> </KCell>
+      </KBlock>
+      <KBlock>
+        <KCell> </KCell>
+      </KBlock>
+    </KSection>
+  </KPage>
+</template>
