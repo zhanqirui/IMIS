897ce86c29085169deed188d85336aee70a21a84
wuxichuan <3431549462@qq.com>
2024-12-24 03:18:13 +0800

    finish big model

--- src/views/msdata/medical-record-info/form.vue
+++ src/views/msdata/medical-record-info/form.vue
@@ -86,10 +86,13 @@ import { ref, toRef } from "vue";
 import { useSelect, useDialogForm } from "@@/plugin-platform/utils/hooks";
 import { required } from "@@/plugin-platform/utils/validators";
 import { addApi, updateApi, detailApi } from './utils/api'
+import { reactive,onMounted } from 'vue';
 
 defineOptions({ inheritAttrs: false })
 
-
+const form_1 = reactive({
+  text: 'dsdsds' // 初始化文本为空字符串
+});
 
 const props = defineProps({
   id: { type: String },
@@ -127,12 +130,57 @@ defineExpose({ submit, reset, closeDialog });
 </script>
 
 <script>
+
+
 export default {      
   methods: {
     goToHome() {
       this.$router.push('/to_treatment_view');
   
+    },
+    setRandomText() {
+      const texts = ['Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5'];
+      const index = Math.floor(Math.random() * texts.length);
+      this.form_1.text = texts[index];
     }
+  },
+  data(){
+    return {
+      // 初始值为预设的文字内容
+      form_1: {
+        text: 'dsdsds' // 初始化文本为空字符串
+      }
+    };
+  },
+  mounted() {
+    this.setRandomText(); // 组件挂载时设置随机文本
   }
+  // setup() {
+  //   const form_1 = reactive({
+  //     text: 'dsdsds' // 初始化文本为空字符串
+  //   });
+
+  //   // 所有可能的文本选项
+  //   const texts = ['Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5'];
+
+  //   // 从texts数组中随机选择一个文本
+  //   function getRandomText() {
+  //     const index = Math.floor(Math.random() * texts.length);
+  //     return texts[index];
+  //   };
+
+  //   // 在组件挂载时设置随机文本
+  //   onMounted(() => {
+  //     form_1.text = getRandomText();
+  //   });
+  //   // function setRandomText() {
+  //   //   form_1.text = getRandomText();
+  //   // }
+
+  //   return {
+  //     form_1
+  //   };
+  // }
 };
 </script>
+
